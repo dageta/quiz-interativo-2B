30edefe3d4a685b3a3e8c38015742c595702e50c
dageta <davi.linhares.nascimento@escola.pr.gov.br>
2025-08-27 07:43:46 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,97 @@
-
+const perguntas = [
+     {
+        enunciado: "perguntas 1",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+            {
+                texto: "alternativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+        ]
+    },
+     {
+        enunciado: "perguntas 2",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+            {
+                texto: "alternativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+        ]
+    },
+     {
+        enunciado: "perguntas 3",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+            {
+                texto: "alternativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+        ]
+    },
+     {
+        enunciado: "perguntas 4",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+            {
+                texto: "alternativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+        ]
+    },
+     {
+        enunciado: "perguntas 5",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+            {
+                texto: "alternativa 2",
+                afirmacao: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+        ]
+    },
+]
